34d12acc920297c85f45213c12459fa1c204bb90
Corey Cossentino <corey@omniti.com>
2014-11-14 16:06:12 -0500

    MIST-149: add config option for statsd host/port

--- stats/stats.go
+++ stats/stats.go
@@ -2,13 +2,11 @@ package stats
 
 import (
 	"fmt"
+	"github.com/mistifyio/mistify-agent/log"
 	"net"
 )
 
-var (
-	stats      = make(chan string)
-	remoteAddr *net.TCPAddr
-)
+var stats = make(chan string)
 
 func Counter(key string, val int) {
 	AddStat("%s:%d|c", key, val)
@@ -34,8 +32,13 @@ func AddStat(format string, params ...interface{}) {
 	stats <- fmt.Sprintf(format, params...)
 }
 
-func Flush() error {
-	conn, err := net.DialTCP("tcp", nil, remoteAddr)
+func Send(addr string) error {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		return err
 	}
@@ -44,7 +47,12 @@ func Flush() error {
 	for stat := range stats {
 		_, err := conn.Write([]byte(stat))
 		if err != nil {
-			return nil
+			log.Error("Couldn't send to statsd: %s. Reconnecting.\n", err.Error())
+
+			conn, err = net.DialTCP("tcp", nil, tcpAddr)
+			if err != nil {
+				log.Error("Couldn't reconnect to statsd: %s.\n", err.Error())
+			}
 		}
 	}
 
