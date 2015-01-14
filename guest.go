2ef396a88eeb5b22eddb521bd9b5ab829cc4aa71
Brian Bickerton <bbickerton@omniti.com>
2015-01-14 16:53:23 +0000

    MIST-246 Allow a guest uuid to be specified on create

--- guest.go
+++ guest.go
@@ -96,10 +96,11 @@ func createGuest(r *HttpRequest) *HttpErrorMessage {
 	if err != nil {
 		return r.NewError(err, 400)
 	}
-	if g.Id != "" {
-		return r.NewError(fmt.Errorf("id must not be set"), 400)
+	if g.Id != "" && uuid.Parse(g.Id) == nil {
+		return r.NewError(fmt.Errorf("id must be uuid"), 400)
+	} else {
+		g.Id = uuid.New()
 	}
-	g.Id = uuid.New()
 
 	// TODO: make sure it's actually unique
 	g.State = "create"
